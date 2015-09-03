ecdb94ddcfb5c604b5dd53045d7ac474263e0e53
Gustav Jorlöv <gustav.jorlov@kits.se>
2015-09-03 11:51:34 +0200

    made the weather service work more natural and will be easier to do more things with

--- server/smhi.js
+++ server/smhi.js
@@ -1,44 +1,51 @@
 var request = require("request");
-var url = "http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.933/lon/12.533/data.json";
+var url = "http://opendata-download-metfcst.smhi.se/api/category" +
+	"/pmp1.5g/version/1/geopoint/lat/57.933/lon/12.533/data.json";
 
 function getWeather(cb){
 	request.get(url, function(err, response, body){
 		var weatherTemp = [];
 		var parsedBody = JSON.parse(body);
 
-		var futureData = getFutureData(parsedBody.timeseries);
+		// var futureData = getFutureData(parsedBody.timeseries);
+		var futureData = parsedBody.timeseries.filter(getFutureData).map(makeNiceData);
 
-		aggregateIntervals(futureData);
+		weatherTemp = aggregateIntervals(futureData);
 
-		for(var i in futureData){
-			if(i % 4 === 0){
-				var dataPoint = futureData[i];
-				weatherTemp.push({
-					"temperature": dataPoint.t,
-					"time": +new Date(dataPoint.validTime),
-					"windspeed": dataPoint.ws,
-					"winddirection": dataPoint.wd,
-					"totalcloudamount": dataPoint.tcc,
-					"precipitation": dataPoint.pit,
-					"precipitationtype": dataPoint.pcat
-				});
-			}
-			
+		
+		cb(null, futureData.slice(0, 5));
+	});
+}
+
+
+
+function getFutureData(item){
+	return +new Date(item.validTime) > +new Date();
+}
+
+function makeNiceData(item){
+	return {
+		"temperature": item.t,
+		"time": +new Date(item.validTime),
+		"windspeed": item.ws,
+		"winddirection": item.wd,
+		"totalcloudamount": item.tcc,
+		"precipitation": item.pit,
+		"precipitationtype": item.pcat
+	};
+}
+
+function aggregateIntervals(futureData){
+	var tempIntervals = [], oneInterval = {};
+	for(var i in futureData){
+		if(i % 4 === 0){
+			var dataPoint = futureData[i];
+			console.log(dataPoint);
 		}
-		cb(null, weatherTemp.slice(0, 5));
-	});
-}
-
-function getFutureData(data){
-	return data.filter(function(item){
-		return +new Date(item.validTime) > +new Date();
-	});
-}
-
-function aggregateIntervals(data){
-	// console.log(data);
+	}
+	return tempIntervals;
 }
 
 module.exports = {
 	getWeather: getWeather
-}
+};
